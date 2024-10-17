66a7baf9585ffd62b71158536e047ceebb81dd3a
mohamadAlrimi <mohamadrimi12345@gmail.com>
2024-10-17 21:47:05 +0300

    Multiple JSX Slots

--- src/components/Examples.js
+++ src/components/Examples.js
@@ -1,41 +1,47 @@
 import { useState } from "react";
-import TabButton from "./TabButton"
+import TabButton from "./TabButton";
 import { EXAMPLES } from "../data";
 import Section from "./Section";
-export default  function Examples() {
-    const [selectedTopic, setSelectedTopic] = useState();
+import Tabs from "./Tabs";
+
+export default function Examples() {
+  const [selectedTopic, setSelectedTopic] = useState();
   function handelSelect(selectedButton) {
     setSelectedTopic(selectedButton);
     console.log(selectedTopic);
   }
-    return ( <Section title="Examples" id="examples">
-        
-        <menu>
-          <TabButton
-            isSelected={selectedTopic === "components"}
-            onClick={() => handelSelect("components")}
-          >
-            Components
-          </TabButton>
-          <TabButton
-            isSelected={selectedTopic === "jsx"}
-            onClick={() => handelSelect("jsx")}
-          >
-            JSX
-          </TabButton>
-          <TabButton
-            isSelected={selectedTopic === "props"}
-            onClick={() => handelSelect("props")}
-          >
-            Prpos
-          </TabButton>
-          <TabButton
-            isSelected={selectedTopic === "state"}
-            onClick={() => handelSelect("state")}
-          >
-            State
-          </TabButton>
-        </menu>
+  return (
+    <Section title="Examples" id="examples">
+      <Tabs
+        buttons={
+          <>
+            <TabButton
+              isSelected={selectedTopic === "components"}
+              onClick={() => handelSelect("components")}
+            >
+              Components
+            </TabButton>
+            <TabButton
+              isSelected={selectedTopic === "jsx"}
+              onClick={() => handelSelect("jsx")}
+            >
+              JSX
+            </TabButton>
+            <TabButton
+              isSelected={selectedTopic === "props"}
+              onClick={() => handelSelect("props")}
+            >
+              Prpos
+            </TabButton>
+            <TabButton
+              isSelected={selectedTopic === "state"}
+              onClick={() => handelSelect("state")}
+            >
+              State
+            </TabButton>
+          </>
+        }
+      >
         {!selectedTopic ? (
           <p>Please select a topic.</p>
         ) : (
@@ -47,5 +53,8 @@ export default  function Examples() {
             </pre>
           </div>
         )}
-      </Section>);
+      </Tabs>
+      <menu></menu>
+    </Section>
+  );
 }
